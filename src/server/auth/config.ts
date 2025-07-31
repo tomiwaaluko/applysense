5e9846eec56a51755b49eb85b8ef1ea4fed8c666
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-07-30 21:26:18 -0400

    minor changes

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "~/server/db";
 
@@ -33,6 +34,10 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     DiscordProvider,
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
     /**
      * ...add more providers here.
      *
