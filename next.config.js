c45262e77a39449c4aba22b407a24b674a0e9277
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-08-08 23:39:44 -0400

    fixed small error

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default config;
